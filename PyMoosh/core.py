f92ed05c239972ab431f3f0b5e519e329234fb35
Antoine Moreau <antoine.moreau@uca.fr>
2023-04-07 00:50:52 +0200

    fieldS added + test.

--- PyMoosh/core.py
+++ PyMoosh/core.py
@@ -490,6 +490,161 @@ def field(struct, beam, window):
 
     return En
 
+def fields(struct, beam, window):
+    """Computes the electric (TE polarization) or magnetic (TM) field inside
+    a multilayered structure illuminated by a gaussian beam, and uses them
+    to compute the rest of the fields (Hx,Hz in TE polarization/Ex,Ez in TM).
+    To be precise, the derived fields need to be divided by omega*µ0 in TE
+    and omega*epsilon_0 in TM. There a missing - sign in TM, too.
+
+    Args:
+        struct (Structure): description (materials,thicknesses)of the multilayer
+        beam (Beam): description of the incidence beam
+        window (Window): description of the simulation domain
+
+    Returns:
+        En (np.array): a matrix with the complex amplitude of the field
+        Hxn (np.array): a matrix with the complex amplitude of Hx/Ex
+        Hzn (np.array): a matrix with the complex amplitude of Hz/Ez
+
+    Afterwards the matrix may be used to represent either the modulus or the
+    real part of the field.
+    """
+
+    # Wavelength in vacuum.
+    lam = beam.wavelength
+    # Computation of all the permittivities/permeabilities
+    Epsilon, Mu = struct.polarizability(lam)
+    thickness = np.array(struct.thickness)
+    w = beam.waist
+    pol = beam.polarization
+    d = window.width
+    theta = beam.incidence
+    C = window.C
+    ny = np.floor(thickness / window.py)
+    nx = window.nx
+    Type = struct.layer_type
+    print("Pixels vertically:", int(sum(ny)))
+
+    # Number of modes retained for the description of the field
+    # so that the last mode has an amplitude < 1e-3 - you may want
+    # to change it if the structure present reflexion coefficients
+    # that are subject to very swift changes with the angle of incidence.
+
+    nmod = int(np.floor(0.83660 * d / w))
+
+    # ----------- Do not touch this part ---------------
+    l = lam / d
+    w = w / d
+    thickness = thickness / d
+
+    if pol == 0:
+        f = Mu
+    else:
+        f = Epsilon
+    # Wavevector in vacuum, no dimension
+    k0 = 2 * pi / l
+    # Initialization of the field component
+    En = np.zeros((int(sum(ny)), int(nx)))
+    Hxn = np.zeros((int(sum(ny)), int(nx)))
+    Hzn = np.zeros((int(sum(ny)), int(nx)))
+    # Total number of layers
+    # g=Type.size-1
+    g = len(struct.layer_type) - 1
+    # Amplitude of the different modes
+    nmodvect = np.arange(-nmod, nmod + 1)
+    # First factor makes the gaussian beam, the second one the shift
+    # a constant phase is missing, it's just a change in the time origin.
+    X = np.exp(-w ** 2 * pi ** 2 * nmodvect ** 2) * np.exp(
+        -2 * 1j * pi * nmodvect * C)
+
+    # Scattering matrix corresponding to no interface.
+    T = np.zeros((2 * g + 2, 2, 2), dtype=complex)
+    T[0] = [[0, 1], [1, 0]]
+    for nm in np.arange(2 * nmod + 1):
+
+        alpha = np.sqrt(Epsilon[Type[0]] * Mu[Type[0]]) * k0 * sin(
+            theta) + 2 * pi * (nm - nmod)
+        gamma = np.sqrt(
+            Epsilon[Type] * Mu[Type] * k0 ** 2 - np.ones(g + 1) * alpha ** 2)
+
+        if np.real(Epsilon[Type[0]]) < 0 and np.real(Mu[Type[0]]) < 0:
+            gamma[0] = -gamma[0]
+
+        if g > 2:
+            gamma[1:g - 1] = gamma[1:g - 1] * (
+                    1 - 2 * (np.imag(gamma[1:g - 1]) < 0))
+        if np.real(Epsilon[Type[g]]) < 0 and np.real(
+                Mu[Type[g]]) < 0 and np.real(
+            np.sqrt(Epsilon[Type[g]] * k0 ** 2 - alpha ** 2)) != 0:
+            gamma[g] = -np.sqrt(
+                Epsilon[Type[g]] * Mu[Type[g]] * k0 ** 2 - alpha ** 2)
+        else:
+            gamma[g] = np.sqrt(
+                Epsilon[Type[g]] * Mu[Type[g]] * k0 ** 2 - alpha ** 2)
+
+        gf = gamma / f[Type]
+        for k in range(g):
+            t = np.exp(1j * gamma[k] * thickness[k])
+            T[2 * k + 1] = np.array([[0, t], [t, 0]])
+            b1 = gf[k]
+            b2 = gf[k + 1]
+            T[2 * k + 2] = np.array([[b1 - b2, 2 * b2],
+                                    [2 * b1, b2 - b1]]) / (b1 + b2)
+        t = np.exp(1j * gamma[g] * thickness[g])
+        T[2 * g + 1] = np.array([[0, t], [t, 0]])
+
+        H = np.zeros((len(T) - 1, 2, 2), dtype=complex)
+        A = np.zeros((len(T) - 1, 2, 2), dtype=complex)
+
+        H[0] = T[2 * g + 1]
+        A[0] = T[0]
+
+        for k in range(len(T) - 2):
+            A[k + 1] = cascade(A[k], T[k + 1])
+            H[k + 1] = cascade(T[len(T) - k - 2], H[k])
+
+        I = np.zeros((len(T), 2, 2), dtype=complex)
+        for k in range(len(T) - 1):
+            I[k] = np.array(
+                [[A[k][1, 0], A[k][1, 1] * H[len(T) - k - 2][0, 1]],
+                 [A[k][1, 0] * H[len(T) - k - 2][0, 0],
+                  H[len(T) - k - 2][0, 1]]] / (
+                        1 - A[k][1, 1] * H[len(T) - k - 2][0, 0]))
+
+        h = 0
+        t = 0
+
+        E = np.zeros((int(np.sum(ny)), 1), dtype=complex)
+        Hx = np.zeros((int(np.sum(ny)), 1), dtype=complex)
+        Hz = np.zeros((int(np.sum(ny)), 1), dtype=complex)
+        for k in range(g + 1):
+            for m in range(int(ny[k])):
+                h = h + float(thickness[k]) / ny[k]
+                #The expression for the field used here is based on the assumption
+                # that the structure is illuminated from above only, with an Amplitude
+                # of 1 for the incident wave. If you want only the reflected
+                # field, take off the second term.
+                E[t, 0] = I[2 * k][0, 0] * np.exp(1j * gamma[k] * h) + \
+                          I[2 * k + 1][1, 0] * np.exp(\
+                    1j * gamma[k] * (thickness[k] - h))
+                Hx[t, 0] = - gf[k] *(I[2 * k][0, 0] * np.exp(1j * gamma[k] * h) - \
+                        I[2 * k + 1][1, 0] * np.exp(\
+                        1j * gamma[k] * (thickness[k] - h)))
+                Hz[t,0] = alpha * E[t,0] / f[Type[k]]
+                t += 1
+
+            h = 0
+        E = E * np.exp(1j * alpha * np.arange(0, nx) / nx)
+        Hx = Hx * np.exp(1j * alpha * np.arange(0, nx) / nx)
+        Hz = Hz * np.exp(1j * alpha * np.arange(0, nx) / nx)
+
+        En = En + X[int(nm)] * E
+        Hxn = Hxn + X[int(nm)] * Hx
+        Hzn = Hzn + X[int(nm)] * Hz
+
+    return En,Hxn,Hzn
+
 
 def Angular(structure, wavelength, polarization, theta_min, theta_max,
             n_points):
---
+++ tests/test_fields.py
@@ -0,0 +1,28 @@
+import numpy as np
+from context import PM
+import matplotlib.pyplot as plt
+materials = []
+
+wavelength = 600
+kr = PM.Structure([1.,'Gold','Water',1.46**2,1.7**2],[4,1,0],[500,40,500])
+polarization = 0.
+incidence,r,t,R,T=PM.Angular(kr,wavelength,polarization,0.,80.,400)
+plt.plot(incidence,R)
+plt.show()
+
+window = PM.Window(70*wavelength,0.4,5.,5.)
+beam = PM.Beam(wavelength,38.7/180*np.pi,polarization,10*wavelength)
+E,Hx,Hz=PM.fields(kr,beam,window)
+plt.figure(2)
+plt.imshow(abs(E),cmap='jet',extent=[0,window.width,0,sum(kr.thickness)],aspect='auto')
+plt.colorbar()
+plt.show()
+
+
+plt.imshow(abs(Hx),cmap='jet',extent=[0,window.width,0,sum(kr.thickness)],aspect='auto')
+plt.colorbar()
+plt.show()
+
+plt.imshow(abs(Hz),cmap='jet',extent=[0,window.width,0,sum(kr.thickness)],aspect='auto')
+plt.colorbar()
+plt.show()
