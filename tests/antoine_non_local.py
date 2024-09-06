163ba45eeb7fa8791780b63d8d9d71e35350eb87
Denis Langevin <denis.langevin@polytechnique.edu>
2024-09-06 17:31:20 +0200

    personal modif to non_local test

--- tests/antoine_non_local.py
+++ tests/antoine_non_local.py
@@ -5,11 +5,12 @@ import csv
 from PyMoosh.non_local import *
 
 wavelength_list = np.linspace(5000, 8000, 3001)
-with open('nlplot.data', newline='') as csvfile:
+with open('tests/nlplot.data', newline='') as csvfile:
     plot = list(csv.reader(csvfile))[0]
 plot = [float(p.strip()) for p in plot]
-plot = np.array(plot)
+plot = np.array(plot)-plot[0]
 plt.plot(wavelength_list, plot, label="ref")
+# base_line = plot[0]
 #plt.show()
 
 def dopedsc_basic(wavelength):
@@ -80,7 +81,11 @@ def sc_2ndviscosity(wavelength, chi_b, w_p, gamma, beta_0, tau):
 
     return eta2, chi_b, chi_f, w_p
 
-nSC_2nd = NLMaterial([sc_2ndviscosity,11.6644913,8.9e+14,3.53201120e+12,np.sqrt(1.92864068e+30),9.718e14])
+chi_b = 11.6644913
+w_p = 8.9e+14
+gamma = 3.53201120e+12
+beta2 = 1.92864068e+30
+nSC_2nd = NLMaterial([sc_2ndviscosity,chi_b,w_p,gamma,np.sqrt(beta2),9.718e14])
 
 advanced_nl = NLStructure([15.6816, nSC_2nd, 14.2129],stack,thickness)
 R_advanced = []
@@ -90,42 +95,47 @@ for wl in wavelength_list:
 R=np.array(R_advanced)
 R = R/max(R)*max(plot)
 plt.plot(wavelength_list,R,label="Advanced NL")
-
+plt.xlabel("Wavelength (nm)")
+plt.ylabel("Reflectivity")
 plt.legend()
+plt.savefig("Fig/Reflectivity_NL_chi_comparison.svg")
+
 plt.show()
 
-def cost_function(X):
-    chi_b, w_p, gamma, beta_0, tau, base, scale = X
-    sc_nl = NLMaterial([sc_2ndviscosity, chi_b, w_p, gamma, beta_0, tau])
-    nb_lam = 50
-    materials = [15.6816, sc_nl, 14.2129]
-    stack = [0, 1, 2]
-    thickness = [0, 105, 0]
-    theta = np.pi * 37 / 180
-    pol = 1.0
-    thing = NLStructure(materials,stack,thickness, verbose=False)
-    wl, r, t, R, T = PM.spectrum(thing, theta, pol, 5000, 8000, nb_lam)
-    R = R*scale + base
-    new_wl = np.linspace(5000, 8000, nb_lam)
-    obj  = np.interp(new_wl, wavelength_list, plot)
-    cost = np.mean(np.abs(R - obj))+10*np.mean(np.abs(np.diff(R)-np.diff(obj)))
-    return cost/nb_lam
+# def cost_function(X):
+#     chi_b, w_p, gamma, beta_0, tau, base, scale = X
+#     sc_nl = NLMaterial([sc_2ndviscosity, chi_b, w_p, gamma, beta_0, tau])
+#     nb_lam = 50
+#     materials = [15.6816, sc_nl, 14.2129]
+#     stack = [0, 1, 2]
+#     thickness = [0, 105, 0]
+#     theta = np.pi * 37 / 180
+#     pol = 1.0
+#     thing = NLStructure(materials,stack,thickness, verbose=False)
+#     wl, r, t, R, T = PM.spectrum(thing, theta, pol, 5000, 8000, nb_lam)
+#     R = R*scale + base
+#     new_wl = np.linspace(5000, 8000, nb_lam)
+#     obj  = np.interp(new_wl, wavelength_list, plot)
+#     cost = np.mean(np.abs(R - obj))+10*np.mean(np.abs(np.diff(R[:,0])-np.diff(obj)))
+#     return cost/nb_lam
 
-X_min = np.array([11.4, 1e14, 1e12, 1e14, 5e14, 0, 0.01])
-X_max = np.array([11.8, 1e15, 1e13, 1.5e15, 1.2e15, 1, 2])
+# X_min = np.array([11.4, 1e14, 1e12, 1e14, 5e14, 0, 0.01])
+# X_max = np.array([11.8, 1e15, 1e13, 1.5e15, 1.2e15, 1, 2])
 
-best,convergence = PM.QODE(cost_function,2000,X_min,X_max,progression=10)
-chi_b, w_p, gamma, beta_0, tau, base, scale = best
+# best,convergence = PM.QODE(cost_function,2000,X_min,X_max,progression=10)
+# chi_b, w_p, gamma, beta_0, tau, base, scale = best
 
-nSC_2nd = NLMaterial([sc_2ndviscosity,chi_b, w_p, gamma, beta_0, tau])
-optimized_nl = NLStructure([15.6816, nSC_2nd, 14.2129],stack,thickness)
-R_optim = []
-for wl in wavelength_list:
-    _,_,R,_ = NLcoefficient(optimized_nl,wl,theta,pol)
-    R_optim.append(R)
-R=np.array(R_optim)
-R = R/max(R)*max(plot)
-plt.plot(wavelength_list,R,label="Optimized NL")
-
-plt.legend()
-plt.show()
+# nSC_2nd = NLMaterial([sc_2ndviscosity,chi_b, w_p, gamma, beta_0, tau])
+# optimized_nl = NLStructure([15.6816, nSC_2nd, 14.2129],stack,thickness)
+# R_optim = []
+# for wl in wavelength_list:
+#     _,_,R,_ = NLcoefficient(optimized_nl,wl,theta,pol)
+#     R_optim.append(R)
+# R=np.array(R_optim)
+# R = R/max(R)*max(plot)
+# plt.plot(wavelength_list,R,label="Optimized NL")
+# plt.xlabel("Wavelength (nm)")
+# plt.ylabel("Reflectivity")
+# plt.legend()
+# plt.savefig("Fig/Non_local_optimized")
+# plt.show()
