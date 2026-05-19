30633227d9ab4f62da57faceca47d27201b59387
Dratsa <aidan.costard8@orange.fr>
2026-05-19 13:45:03 +0200

    non_spec.py

--- PyMoosh/non_spec.py
+++ PyMoosh/non_spec.py
@@ -16,14 +16,12 @@ def profil_R(struct, beam, window, type = "abs"):
     Returns:
         Field (Array): return the reflected field along the first interface
     """
-    E = field(struct, beam, window, True)
-    layer = struct.thickness
-    Pixel_layer = abs(int(layer[0]/window.py) - 1)
+    E = NSfield(struct, beam, window, True)
     if type == "abs":
-        inter_up = abs(E[Pixel_layer])
+        inter = abs(E)
     else:
-        inter_up = E[Pixel_layer]
-    return inter_up
+        inter = E
+    return inter
 
 #Profil of the incoming wave amplitude along the first surface layer
 def profil_In(struct, beam, window, type = "abs"):
@@ -37,18 +35,15 @@ def profil_In(struct, beam, window, type = "abs"):
     Returns:
         Field (Array): return the incident field along the first interface
     """
-    E = field(struct, beam, window, False)
-    layer = struct.thickness
-    Pixel_layer = abs(int(layer[0]/window.py) - 1)
-    #-1 because the intern reflexions at the interface counts as incoming beam, we take the "upper" interface to keep only the true incoming beam
+    E = NSfield(struct, beam, window, False)
     if type == "abs":
-        inter_up = abs(E[Pixel_layer])
+        inter = abs(E)
     else:
-        inter_up = E[Pixel_layer]
-    return inter_up
+        inter = E
+    return inter
 
 #Profil of the wave amplitude along the first surface layer (in all directions)
-def profil(struct, beam, window, type = "abs"):
+def profil_tot(struct, beam, window, type = "abs"):
     """
     Args:
         struct (Structure): the structure used
@@ -59,25 +54,23 @@ def profil(struct, beam, window, type = "abs"):
     Returns:
         Field (Array): return the field along the first interface
     """
-    E = field(struct, beam, window)
-    layer = struct.thickness
-    Pixel_layer = abs(int(layer[0]/window.py) - 1)
+    E = NSfield(struct, beam, window)
     if type == "abs":
-        inter_up = abs(E[Pixel_layer])
+        inter = abs(E)
     else:
-        inter_up = E[Pixel_layer]
-    return inter_up
+        inter = E
+    return inter
 
-def deltas(struct, beam, window):
+def deltas(struct, beam, window, rel = True):
     """
     Args:
         struct (Structure): the structure used
         beam (Beam): the beam used
         window (Window): the window used (big enough to avoid resonances during computation using the function "field")
-
+        rel (Boolean): "True" by default. If rel is "True" the function will return relative shift and enlargement. If not it will return the algebric values
     Returns:
-        delta (float): the shift between the center of the incident and the reflected beam
-        Delta (float): the enlargement of the reflected beam compared to the incident beam
+        delta (float): the shift of the reflected beam compared to the incident beam (relative if rel is "True", algebric otherwise)
+        Delta (float): the enlargement of the reflected beam compared to the incident beam (relative if rel is "True", algebric otherwise)
     """
     midIn = 0
     midOut = 0
@@ -94,7 +87,10 @@ def deltas(struct, beam, window):
     varOut = np.trapezoid((X - np.ones(len(X)) * (midIn + delta))**2 * np.abs(outer)**2)/np.trapezoid(np.abs(outer)**2)
     Delta = varOut - varIn
     
-    return float(delta), float(Delta)
+    if rel:
+        return float(delta) / beam.waist, varOut / beam.waist
+    else:
+        return float(delta), float(Delta)
 
 def asymptcoef(struct, beam):
     """
@@ -103,7 +99,7 @@ def asymptcoef(struct, beam):
         beam (Beam): the beam used
 
     Returns:
-        delta (float):
+        delta (float): the shift of the reflected beam compared to the incident beam
         Delta (float): the enlargement of the reflected beam compared to the incident beam
     """
     lam = beam.wavelength
@@ -122,8 +118,6 @@ def asymptcoef(struct, beam):
         # kx = n*ko*sin(theta) so d/d(kx) = d/(alpha * d(theta))  = 1/alpha * d/d(theta)
     c0 = nk0 * np.cos(theta)
     phase = np.array(phase)
-    # We have to supressed the 2*pi jumped in the angular list (especially around limit angle) so by using unwrap we will get a better continuity (useful for the gradient)
-    phase = np.unwrap(phase) 
     # Because of the size of the array we must redefine dtheta
     dtheta *= 2 / (N-1)
     dphase = np.gradient((phase), dtheta)/(c0)
@@ -140,3 +134,149 @@ def asymptcoef(struct, beam):
     dR2 = dc0 * dR + ddR / c0**2
     widthlim = (dR**2 / (c0 * R)**2 - dR2 / R) / 2
     return -float(np.real(dphase[int(N/2)])), float(np.real(widthlim))
+
+def NSfield(struct, beam, window, onlyreflected = None):
+    """Computes the electric (TE polarization) or magnetic (TM) field inside
+    a multilayered structure illuminated by a gaussian beam.
+
+    Args:
+        struct (Structure): description (materials,thicknesses)of the multilayer
+        beam (Beam): description of the incidence beam
+        window (Window): description of the simulation domain
+        onlyreflected (None type): None by default, if True it will only shows the reflected field, if False it will only shows the incident field
+
+    Returns:
+        En (np.array): a matrix with the complex amplitude of the field
+
+    Afterwards the matrix may be used to represent either the modulus or the
+    real part of the field.
+    """
+    # Wavelength in vacuum.
+    lam = beam.wavelength
+    # Computation of all the permittivities/permeabilities
+    Epsilon, Mu = struct.polarizability(lam)
+    thickness = np.array(struct.thickness)
+    # Position where the beam waste is taken
+    Dphi = beam.distance
+    w = beam.waist
+    pol = beam.polarization
+    d = window.width
+    theta = beam.incidence
+    pixDphi = Dphi/d
+    C = window.C
+    ny = np.floor(thickness / window.py)
+    nx = window.nx
+    Type = struct.layer_type
+    transmission = 1
+    reflexion = 1
+    if onlyreflected:
+        transmission = 0
+    elif onlyreflected == False:
+        reflexion = 0
+    # Number of modes retained for the description of the field
+    # so that the last mode has an amplitude < 1e-3 - you may want
+    # to change it if the structure present reflexion coefficients
+    # that are subject to very swift changes with the angle of incidence.
+    
+    nmod = int(np.floor(0.83660 * d / w))
+
+    # ----------- Do not touch this part ---------------
+    l = lam / d
+    w = w / d
+    thickness = thickness / d
+
+
+    if pol == 0:
+        f = Mu
+    else:
+        f = Epsilon
+    # Wavevector in vacuum, no dimension
+    k_0 = 2 * pi / l
+    # Initialization of the field component
+    En = []
+    # Total number of layers
+    # g=Type.size-1
+    g = len(struct.layer_type) - 1
+    # Amplitude of the different modes
+    nmodvect = np.arange(-nmod, nmod + 1)
+    # First factor makes the gaussian beam, the second one the shift
+    # a constant phase is missing, it's just a change in the time origin.
+    gauss = np.exp(-(w**2) * pi**2 * nmodvect**2)
+    phase = np.exp(-2 * 1j * pi * nmodvect * C)
+    X = gauss * phase 
+    
+    layer_k = np.sqrt(Epsilon[Type] * Mu[Type] * k_0**2)
+
+    # Scattering matrix corresponding to no interface.
+    T = np.zeros((2 * g + 2, 2, 2), dtype=complex)
+    T[0] = [[0, 1], [1, 0]]
+    for nm in np.arange(2 * nmod + 1):
+
+        n_0 = np.sqrt(Epsilon[Type[0]] * Mu[Type[0]])
+        alpha = n_0 * k_0 * sin(theta) + 2 * pi * (nm - nmod)
+        gamma = np.sqrt(layer_k**2 - np.ones(g + 1) * alpha**2)
+
+        if np.real(Epsilon[Type[0]]) < 0 and np.real(Mu[Type[0]]) < 0:
+            gamma[0] = -gamma[0]
+
+        if g > 2:
+            im_sign = np.imag(gamma[1 : g - 1]) < 0
+            gamma[1 : g - 1] = gamma[1 : g - 1] * (1 - 2 * im_sign)
+        if (
+            np.real(Epsilon[Type[g]]) < 0
+            and np.real(Mu[Type[g]]) < 0
+            and np.real(np.sqrt(layer_k[g] ** 2 - alpha**2)) != 0
+        ):
+            gamma[g] = -np.sqrt(layer_k[g] ** 2 - alpha**2)
+        else:
+            gamma[g] = np.sqrt(layer_k[g] ** 2 - alpha**2)
+        
+        # Suppression of vanishing mod when the waist distance is not 0, to avoid amplifications and overwhelming amplitudes 
+        if (np.imag(gamma[0]) > 0) and (pixDphi > 0):
+            gamma[0] = 0
+        
+        
+        gf = gamma / f[Type]
+        for k in range(g):
+            t = np.exp(1j * gamma[k] * (thickness[k]))
+            T[2 * k + 1] = np.array([[0, t], [t, 0]])
+            b1 = gf[k]
+            b2 = gf[k + 1]
+            T[2 * k + 2] = np.array([[b1 - b2, 2 * b2], [2 * b1, b2 - b1]]) / (b1 + b2)
+        t = np.exp(1j * gamma[g] * thickness[g])
+        T[2 * g + 1] = np.array([[0, t], [t, 0]])
+
+        H = np.zeros((len(T) - 1, 2, 2), dtype=complex)
+        A = np.zeros((len(T) - 1, 2, 2), dtype=complex)
+
+        H[0] = T[2 * g + 1]
+        A[0] = T[0]
+
+        for k in range(len(T) - 2):
+            A[k + 1] = cascade(A[k], T[k + 1])
+            H[k + 1] = cascade(T[len(T) - k - 2], H[k])
+
+        I = np.zeros((2, 2, 2), dtype=complex)
+        for k in range(2):
+            I[k] = np.array(
+                [
+                    [A[k][1, 0], A[k][1, 1] * H[len(T) - k - 2][0, 1]],
+                    [A[k][1, 0] * H[len(T) - k - 2][0, 0], H[len(T) - k - 2][0, 1]],
+                ]
+                / (1 - A[k][1, 1] * H[len(T) - k - 2][0, 0])
+            )
+
+        h = 0
+
+        E = 0
+
+        for m in range(int(ny[0])):
+            h += float(thickness[0]) / ny[0]
+        E = transmission*(I[0][0, 0] * np.exp(1j * gamma[0] * h)) + reflexion*(I[1][
+            1, 0] * np.exp(1j * gamma[0] * (thickness[0] - h)))
+        E = E * np.exp(1j * alpha * np.arange(0, nx) / nx) 
+        # We can now add the phase on gamma for the waist distance (we use gamma[0] because the phase is implemented in the first layer)
+        E *= np.exp(-1j * gamma[0] * pixDphi)
+        En.append(X[int(nm)] * E) 
+
+    return sum(En)
